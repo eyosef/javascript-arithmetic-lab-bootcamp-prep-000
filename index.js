4f69207c50adf7b84ed3ac7b17f4c99d603b0115
Eden Mesfun <emesfun@gmail.com>
2017-12-18 19:03:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-//answer 1 (test passed)
+//answer 1
 function add(a,b)
 {
   if (a+b === a+b)
   {return(a+b)}
 }
 
-//answer 2 (test passed)
+//answer 2
 function subtract (a,b)
 {
   if (a-b === a-b)
   {return (a-b)}
 }
 
-//answer 3 (test passed)
+//answer 3
 function multiply (a,b)
 {
   if (a*b === a*b)
   {return (a*b)}
 }
 
-//answer 4 (test passed)
+//answer 4 
 function divide(a,b)
 {
   if (a/b === a/b)
   {return (a/b)}
 }
 
-//answer 5 (test passed)
+//answer 5 
 function inc(n)
 { 
   if (n === n)
   {return (n+1)}
 }
 
-//answer 6 (test passed)
+//answer 6 
 function dec(n)
 {
  if (n === n)
  {return(n-1)}
 }
 
-//answer 7 (test passed)
+//answer 7 
 function makeInt(n)
 {
   {return(parseInt(n, 10))}
