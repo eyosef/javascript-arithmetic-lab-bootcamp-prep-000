c670c151effcb225ee26294ee96337f2e3472552
Eden Mesfun <emesfun@gmail.com>
2017-12-18 18:28:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,51 @@
+//answer 1 (test passed)
+function add(a,b)
+{
+  if (a+b === a+b)
+  {return(a+b)}
+}
+
+//answer 2 (test passed)
+function subtract (a,b)
+{
+  if (a-b === a-b)
+  {return (a-b)}
+}
+
+//answer 3 (test passed)
+function multiply (a,b)
+{
+  if (a*b === a*b)
+  {return (a*b)}
+}
+
+//answer 4 (test passed)
+function divide(a,b)
+{
+  if (a/b === a/b)
+  {return (a/b)}
+}
+
+//answer 5 (test passed)
+function inc(n)
+{ 
+  if (n === n)
+  {return (n+1)}
+}
+
+//answer 6 (test passed)
+function dec(n)
+{
+ if (n === n)
+ {return(n-1)}
+}
+
+//answer 7 (test passed)
+function makeInt(n)
+{
+  if (n === n)
+  {return(parseInt(n))}
+  
+  else if (`sldkjflksjf` === true)
+  {return(NaN)} 
+}
